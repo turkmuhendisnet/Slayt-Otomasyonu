f8726c26b82878c58717ff54d21f5fe6dfc6d398
Turkmuhendisnet <48014368+turkmuhendisnet@users.noreply.github.com>
2019-08-03 12:19:02 +0300

    Slayt Otomasyonu
    Yüklenen resimler ile slay hazırlayıp gösteren program.

---
+++ ResimSlayt/ResimSlayt/ResimSlayt/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ResimSlayt
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStarStop = new System.Windows.Forms.Button();
+            this.BtnGeri = new System.Windows.Forms.Button();
+            this.btnilk = new System.Windows.Forms.Button();
+            this.btnİleri = new System.Windows.Forms.Button();
+            this.btnSon = new System.Windows.Forms.Button();
+            this.pctrBxResimler = new System.Windows.Forms.PictureBox();
+            this.tmrSure = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pctrBxResimler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStarStop
+            // 
+            this.btnStarStop.BackColor = System.Drawing.Color.Aqua;
+            this.btnStarStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStarStop.Location = new System.Drawing.Point(208, 361);
+            this.btnStarStop.Name = "btnStarStop";
+            this.btnStarStop.Size = new System.Drawing.Size(75, 34);
+            this.btnStarStop.TabIndex = 1;
+            this.btnStarStop.Text = "Star Stop";
+            this.btnStarStop.UseVisualStyleBackColor = false;
+            this.btnStarStop.Click += new System.EventHandler(this.btnStarStop_Click);
+            // 
+            // BtnGeri
+            // 
+            this.BtnGeri.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnGeri.Location = new System.Drawing.Point(114, 361);
+            this.BtnGeri.Name = "BtnGeri";
+            this.BtnGeri.Size = new System.Drawing.Size(75, 34);
+            this.BtnGeri.TabIndex = 2;
+            this.BtnGeri.Text = "<<";
+            this.BtnGeri.UseVisualStyleBackColor = true;
+            this.BtnGeri.Click += new System.EventHandler(this.BtnGeri_Click);
+            // 
+            // btnilk
+            // 
+            this.btnilk.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnilk.Location = new System.Drawing.Point(12, 361);
+            this.btnilk.Name = "btnilk";
+            this.btnilk.Size = new System.Drawing.Size(75, 34);
+            this.btnilk.TabIndex = 3;
+            this.btnilk.Text = "I<";
+            this.btnilk.UseVisualStyleBackColor = true;
+            this.btnilk.Click += new System.EventHandler(this.btnilk_Click);
+            // 
+            // btnİleri
+            // 
+            this.btnİleri.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnİleri.Location = new System.Drawing.Point(308, 361);
+            this.btnİleri.Name = "btnİleri";
+            this.btnİleri.Size = new System.Drawing.Size(75, 34);
+            this.btnİleri.TabIndex = 4;
+            this.btnİleri.Text = ">>";
+            this.btnİleri.UseVisualStyleBackColor = true;
+            this.btnİleri.Click += new System.EventHandler(this.btnİleri_Click);
+            // 
+            // btnSon
+            // 
+            this.btnSon.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSon.Location = new System.Drawing.Point(410, 361);
+            this.btnSon.Name = "btnSon";
+            this.btnSon.Size = new System.Drawing.Size(75, 34);
+            this.btnSon.TabIndex = 5;
+            this.btnSon.Text = ">I";
+            this.btnSon.UseVisualStyleBackColor = true;
+            this.btnSon.Click += new System.EventHandler(this.btnSon_Click);
+            // 
+            // pctrBxResimler
+            // 
+            this.pctrBxResimler.Image = global::ResimSlayt.Properties.Resources.Resim11;
+            this.pctrBxResimler.Location = new System.Drawing.Point(12, 12);
+            this.pctrBxResimler.Name = "pctrBxResimler";
+            this.pctrBxResimler.Size = new System.Drawing.Size(473, 343);
+            this.pctrBxResimler.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctrBxResimler.TabIndex = 0;
+            this.pctrBxResimler.TabStop = false;
+            // 
+            // tmrSure
+            // 
+            this.tmrSure.Tick += new System.EventHandler(this.tmrSure_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 429);
+            this.Controls.Add(this.btnSon);
+            this.Controls.Add(this.btnİleri);
+            this.Controls.Add(this.btnilk);
+            this.Controls.Add(this.BtnGeri);
+            this.Controls.Add(this.btnStarStop);
+            this.Controls.Add(this.pctrBxResimler);
+            this.Name = "Form1";
+            this.Text = "Slayt Gösterisi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctrBxResimler)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctrBxResimler;
+        private System.Windows.Forms.Button btnStarStop;
+        private System.Windows.Forms.Button BtnGeri;
+        private System.Windows.Forms.Button btnilk;
+        private System.Windows.Forms.Button btnİleri;
+        private System.Windows.Forms.Button btnSon;
+        private System.Windows.Forms.Timer tmrSure;
+    }
+}
+
---
+++ ResimSlayt/ResimSlayt/ResimSlayt/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResimSlayt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Image[] resimler = new Image[10];
+        int RandomSayıKontrol = 0;
+        int tiklama = 0;
+        int sayac =0;
+
+        private void btnStarStop_Click(object sender, EventArgs e)
+        {
+            tiklama++;              
+            if (tiklama%2==1)
+                tmrSure.Enabled = true;           
+            else
+                tmrSure.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resimler[0] = Properties.Resources.Resim1;
+            resimler[1] = Properties.Resources.Resim2;
+            resimler[2] = Properties.Resources.Resim3;
+            resimler[3] = Properties.Resources.Resim4;
+            resimler[4] = Properties.Resources.Resim5;
+            resimler[5] = Properties.Resources.Resim6;
+            resimler[6] = Properties.Resources.Resim7;
+            resimler[7] = Properties.Resources.Resim8;
+            resimler[8] = Properties.Resources.Resim9;
+            resimler[9] = Properties.Resources.Resim10;
+            
+
+            tmrSure.Enabled = false;
+            tmrSure.Interval = 3000;
+
+        }
+
+        private void btnİleri_Click(object sender, EventArgs e)
+        {
+            if (sayac>=9)
+                sayac = 0;
+            else
+                sayac++;
+            pctrBxResimler.Image = resimler[sayac];
+        }
+
+        private void btnSon_Click(object sender, EventArgs e)
+        {
+            pctrBxResimler.Image = resimler[9];
+        }
+
+        private void btnilk_Click(object sender, EventArgs e)
+        {
+            pctrBxResimler.Image = resimler[0];
+        }
+
+        private void BtnGeri_Click(object sender, EventArgs e)
+        {
+            if (sayac <=0)
+                sayac = resimler.Length-1;
+            else
+               sayac--;
+            pctrBxResimler.Image = resimler[sayac];
+        }
+
+        private void tmrSure_Tick(object sender, EventArgs e)
+        {
+            Random RastgeleSayi = new Random();
+            sayac = RastgeleSayi.Next(0, resimler.Length);
+            
+            if (RandomSayıKontrol!=sayac)
+            {
+                pctrBxResimler.Image = resimler[sayac];
+                RandomSayıKontrol = sayac;
+            }
+            else
+            {
+                RandomSayıKontrol++;
+                pctrBxResimler.Image = resimler[sayac];
+
+            }
+        }
+    }
+}
